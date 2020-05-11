e7fdecf40ad9f9ce34cc5c9b75fdd317245d35b1
Gustavo Landfried <gustavolandfried@gmail.com>
2020-05-11 10:57:45 -0300

    Corrige la forma de modificar el dataframe de pandas

--- estimations/tsh.py
+++ estimations/tsh.py
@@ -31,38 +31,40 @@ from collections import defaultdict
 handicap = defaultdict(lambda:env.Rating(0,25/3,0,1/100))
 player = defaultdict(lambda:env.skill())
 
-for i in df.index:#i=0    
-        
-    if df.iloc[i].ranked:
-        es.iloc[i].estimated = True        
+for i in df.index:#i=0            
+    
+    h_key = (df.loc[i].handicap , df.loc[i].width)    
+    
+    if df.loc[i].ranked:
+        es.loc[i,'estimated'] = True        
         # Teams
-        tw = env.Team([player[df.iloc[i].white]])
-        # Con handicap
-        h_key = (df.iloc[i].handicap , df.iloc[i].width)
+        tw = env.Team([player[df.loc[i].white]])
+        # Con handicap    
         if h_key[0] > 1:
-            tb = env.Team([player[df.iloc[i].black],handicap[h_key]])
+            tb = env.Team([player[df.loc[i].black],handicap[h_key]])
         else:
-            tb = env.Team([player[df.iloc[i].black]])
+            tb = env.Team([player[df.loc[i].black]])
         
-        result = [1,0] if df.iloc[i].black_win else [0,1]
+        result = [1,0] if df.loc[i].black_win else [0,1]
         
         game = env.Game([tw,tb],result)
         es.evidence_tsh = game.evidence
         tw_post, tb_post = game.posterior
         
-        es.iloc[i].w_mean, es.iloc[i].w_std = tw_post[0]
-        es.iloc[i].b_mean, es.iloc[i].b_std = tb_post[0]
-        es.iloc[i].h_mean, es.iloc[i].h_std = tb_post[1] if h_key[0] >1 else handicap[h_key]
+        es.loc[i,'w_mean'], es.loc[i,'w_std'] = tw_post[0]
+        es.loc[i,'b_mean'], es.loc[i,'b_std'] = tb_post[0]
+        es.loc[i,'h_mean'], es.loc[i,'h_std'] = tb_post[1] if h_key[0] >1 else handicap[h_key]
         
-        player[df.iloc[i].white] = tw_post[0]
-        player[df.iloc[i].black] = tb_post[0]
+        player[df.loc[i].white] = tw_post[0]
+        player[df.loc[i].black] = tb_post[0]
         handicap[h_key] = tb_post[1] if h_key[0] >1 else handicap[h_key]
         
     else:
         es.iloc[i].estimated_tsh = False
-        es.iloc[i].w_mean, es.iloc[i].w_std = player[df.iloc[i].white]
-        es.iloc[i].b_mean, es.iloc[i].b_std = player[df.iloc[i].black]    
-                
+        es.loc[i,'w_mean'], es.loc[i,'w_std'] = player[df.iloc[i].white]
+        es.loc[i,'b_mean'], es.loc[i,'b_std'] = player[df.iloc[i].black]    
+        es.loc[i,'h_mean'], es.loc[i,'h_std'] = handicap[h_key]
+                    
     print(f"Porcentaje de "+name+".py es del {int(i/len(df.index)*100)}%", end='\r')
      
 df.to_csv(name+".csv", index=False)
