6c06c9c307cc161793e22145cb6c2590b401dc5b
Gustavo Landfried <gustavolandfried@gmail.com>
2020-05-14 16:58:34 -0300

    Change file name, add convergence

--- estimations/ogs/ttt_all.py
+++ estimations/ogs/ttt_all.py
@@ -3,13 +3,14 @@ name = os.path.basename(__file__).split(".py")[0]
 ##################
 #import time
 import pandas as pd
+import pickle
 import sys
 sys.path.append('../../software/trueskill.py/')
 #import numpy as np
 import src as th
 from importlib import reload  # Python 3.4+ only.
 reload(th)
-env = th.TrueSkill(draw_probability=0,tau=1)
+env = th.TrueSkill(draw_probability=0,tau=1,epsilon=0.1)
 
 # Posible variable
 dataset = 'ogs'
@@ -28,9 +29,9 @@ composition = [[[w],[b]] if h<2 else [[w],[b,(h,s)]] for w, b, h, s in zip(df.wh
 history= env.history(composition, results, prior_dict=prior_dict)
 #history.trueSkill()
 history.through_time()
+history.convergence()
 
-import cPickle
 with open(name+".pickle", "wb") as output_file:
-    cPickle.dump(history, output_file)
-   
+    pickle.dump(history, output_file,protocol=pickle.HIGHEST_PROTOCOL)
+
 #es.to_csv(name+".csv", index=False)
