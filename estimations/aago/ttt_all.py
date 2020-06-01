e60b63504261c444aa7d03e2c23257972ae48cdf
Gustavo Landfried <gustavolandfried@gmail.com>
2020-06-01 11:34:46 -0300

    Buscando bug en escritura, aago

--- estimations/aago/ttt_all.py
+++ estimations/aago/ttt_all.py
@@ -23,7 +23,7 @@ fit_mu_handicap = [-0.53445148,  0.82848756]
 prior_dict = {}
 for h_key in set([(h,19) for h in df.handicap ]):
     mu_h = fit_mu_handicap[0] + fit_mu_handicap[1]*h_key[0]
-    prior_dict[h_key] = env.Rating(mu_h,1,0,0)
+    prior_dict[h_key] = env.Rating(mu_h,0.5,0,0)
 
 results = list(df.result.map(lambda x: [1,0] if x=="black" else [0,1] ) )
 composition = [[[w],[b]] if h<2 else [[w],[b,(h,19)]] for w, b, h in zip(df.white_player_id, df.black_player_id, df.handicap) ]   
@@ -32,7 +32,7 @@ batch_number = list(df.event_id)
 
 history= env.history(composition, results,batch_number, prior_dict=prior_dict)
 #history.trueSkill()
-history.through_time(online=False)
+history.through_time(online=True)
 history.convergence()
 
 #ipdb.set_trace()
@@ -43,16 +43,22 @@ history.convergence()
 #        res[str(j)][0].append(t.posterior(j).mu)
 #        res[str(j)][1].append(t.posterior(j).sigma)
 
-w_mean = [t.posteriors[g[0][0]].mu for t in history.times for g in t.games_composition]
-w_std = [t.posteriors[g[0][0]].sigma for t in history.times for g in t.games_composition]
-b_mean = [t.posteriors[g[1][0]].mu for t in history.times for g in t.games_composition]
-b_std = [t.posteriors[g[1][0]].sigma for t in history.times for g in t.games_composition]
-h_mean = [ t.posteriors[g[1][1]].mu if len(g[1]) > 1 else 0 for t in history.times for g in t.games_composition]
-h_std = [ t.posteriors[g[1][1]].sigma if len(g[1]) > 1 else 0 for t in history.times for g in t.games_composition]
-evidence = [e for t in history.times for e in t.last_evidence]
+w_id = [g[0][0] for t in history.times for g in t.games_composition]
+b_id = [g[1][0] for t in history.times for g in t.games_composition]
+h_id = [g[1][1] if len(g[1]) > 1 else -1 for t in history.times for g in t.games_composition]
+w_mean = [t.posterior(g[0][0]).mu for t in history.times for g in t.games_composition]
+w_std = [t.posteriors(g[0][0]).sigma for t in history.times for g in t.games_composition]
+b_mean = [t.posteriors(g[1][0]).mu for t in history.times for g in t.games_composition]
+b_std = [t.posteriors(g[1][0]).sigma for t in history.times for g in t.games_composition]
+h_mean = [ t.posteriors(g[1][1]).mu if len(g[1]) > 1 else 0 for t in history.times for g in t.games_composition]
+h_std = [ t.posteriors(g[1][1]).sigma if len(g[1]) > 1 else 0 for t in history.times for g in t.games_composition]
+evidence = [e for t in history.times for e in t.evidence]
 last_evidence = [e for t in history.times for e in t.last_evidence]
 
 res = df[['id']].copy() 
+res["w_id"] = w_id 
+res["b_id"] = b_id
+res["h_id"] = h_id
 res["w_mean"] = w_mean
 res["w_std"] = w_std
 res["b_mean"] = b_mean
--- figures/aago.py
+++ figures/aago.py
@@ -2,59 +2,40 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from matplotlib.patches import Polygon
-
-import json
+from itertools import groupby
 
 df = pd.read_csv('../data/aago/summary_filtered.csv')
+data = pd.read_csv('../estimations/aago/ttt_all.csv')
 # Opening JSON file 
-f = open('../estimations/aago/ttt_all.json',) 
-data = json.load(f) 
+sum(df.id != data.id)
 
-
-for j in data:
-    
-    if (not "(" in j) and len(data[j][0])//2 > 20:
-        m = data[j][0][0:(len(data[j][0])//2)]
-        plt.plot(m)
-
-for j in data:    
-    if (not "(" in j) and len(data[j][0])//2 > 1 and  len(data[j][0])//2 < 8:
-        m = data[j][0][0:(len(data[j][0])//2)]
-        plt.plot(m)
-
-for j in data:
-    if "(" in j:
-        m = data[j][0][0:(len(data[j][0])//2)]
-        plt.plot(m)
-        
-        x = list(range(len(data[j][0])//2))
-        x = x+ list(reversed(x))
-        y = m + 1* np.array(data[j][1][0:(len(data[j][0])//2)] )
-        y = list(y) + list(reversed(m - 1* np.array(data[j][1][0:(len(data[j][0])//2)] )))
-        plt.plot(m,color="black")
-        plt.fill(x,y)
-    
-
-
-"""        
-for j in data:
-    
-    if (not "(" in j) and len(data[j][0])//2 > 20:
-        m = data[j][0][0:(len(data[j][0])//2)]
-        plt.plot(m,color="black")
-        
-        x = list(range(len(data[j][0])//2))
-        x = x+ list(reversed(x))
-        y = m + 1* np.array(data[j][1][0:(len(data[j][0])//2)] )
-        y = list(y) + list(reversed(m - 1* np.array(data[j][1][0:(len(data[j][0])//2)] )))
-        plt.plot(m,color="black")
-        plt.fill(x,y)
-    
-        
-for j in data:
-    if (not "(" in j) and len(data[j][0])//2 > 20:
-        eventos = list(set(list(df.event_id[(df.white_player_id == int(j)) | (df.black_player_id == int(j))])))
-        plt.plot(eventos, data[j][0][0:(len(data[j][0])//2)])
-
+np.exp(np.sum(np.log(data.evidence))/len(data.evidence))
+np.exp(np.sum(np.log(data.last_evidence))/len(data.evidence))
 
 """
+TODO:
+    1. por qu\'e last evidence es igual a evidence.
+"""
+
+df.columns 
+players = list(df.black_player_id) + list(df.black_player_id)
+activity = [(i, players.count(i)) for i in set(players)]
+
+filtro = (df.white_player_id == 53) | (df.black_player_id == 53)
+data[filtro]
+
+for j,a in activity:
+    filtro = (df.white_player_id == j) | (df.black_player_id == j)
+    if a>20:
+        curva = (df.white_player_id[filtro] == j) * data[filtro].w_mean + (df.black_player_id[filtro] == j) * data[filtro].b_mean
+        plt.plot(range(len(curva )) ,curva )
+    #plt.xlim(0,128)
+    
+filtro = (df.handicap == 3)
+plt.plot(data[filtro].h_mean)
+
+for h in range(2,10):
+    filtro = (df.handicap == h)
+    plt.plot(range(sum(filtro)) ,data[filtro].h_mean)
+    
+    
