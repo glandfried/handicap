a77e0acfcc245035821ea4e8d3922f4c8cc1e626
Martin Amigo <martinamigo@protonmail.com>
2021-05-30 18:21:07 -0300

    agrego resultados a ogs.jl y renombro filtered(_old).txt

--- estimations/ogs.jl
+++ estimations/ogs.jl
@@ -8,7 +8,7 @@ using Dates
 using DataFrames
 
 println("Opening dataset")
-data = CSV.read("../data/ogs/summary_filtered.csv")    
+data = CSV.read("../data/ogs/summary_filtered.csv", DataFrame)
 
 days = Dates.value.(
     Date.(map(m->m.match,
@@ -22,18 +22,20 @@ end
 
 results = [row.black_win == 1 ? [0.,1.] : [1., 0.] for row in eachrow(data) ]
 
-events = [ r.handicap<2 ? [[string(r.white)],[string(r.black)]] : [[string(r.white)],[string(r.black),string((r.handicap,r.width))]] for r in eachrow(data) ]   
+events = [ r.handicap<2 ? [[string(r.white)],[string(r.black)]] : [[string(r.white)],[string(r.black),string((r.handicap,r.width))]] for r in eachrow(data) ]
 
 # gamma = 0.12; iter 4; -193062.34822408648
 # gamma = 0.16; iter 4; -192830.49964918988
 # gamma = 0.18; iter 4; -192991.47740192755
 
-h = missing 
+h = missing
 GC.gc()
 h = ttt.History(composition=events, results=results, times = days , priors=prior_dict, sigma=10.,gamma=0.18)
 ts_log_evidence = ttt.log_evidence(h)
 ttt.convergence(h, iterations=4)
 ttt_log_evidence = ttt.log_evidence(h)
+println("Evidencia con handicap:")
+println(ttt_log_evidence)
 
 lc = ttt.learning_curves(h)
 
@@ -41,24 +43,31 @@ for (k,v) in prior_dict
     if haskey(lc,k)
         println(k,lc[k][end][2])
     end
-end    
+end
 
 
 for h_key in Set([(row.komi, row.width) for row in eachrow(data) ])
     prior_dict[string(h_key)] = ttt.Player(ttt.Gaussian(0.0,6.0),0.0,0.0)
 end
 
-events = [ r.handicap<2 ? [[string(r.white), string((r.komi,r.width))],[string(r.black)]] : [[string(r.white), string((r.komi,r.width))],[string(r.black),string((r.handicap,r.width))]] for r in eachrow(data) ]   
+events = [ r.handicap<2 ? [[string(r.white), string((r.komi,r.width))],[string(r.black)]] : [[string(r.white), string((r.komi,r.width))],[string(r.black),string((r.handicap,r.width))]] for r in eachrow(data) ]
 
-
+# Komi; gamma = 0.14; iter 4; -192707
 # Komi; gamma = 0.16; iter 4; -192684
+# Komi; gamma = 0.18; iter 4; -192828
 
-h = missing 
-GC.gc()
-h = ttt.History(composition=events, results=results, times = days , priors=prior_dict, sigma=10.,gamma=0.16)
-ts_log_evidence = ttt.log_evidence(h)
-ttt.convergence(h, iterations=4)
-ttt_log_evidence = ttt.log_evidence(h)
+for gamma in [0.14, 0.16, 0.18]
+    h = missing
+    GC.gc()
+    h = ttt.History(composition=events, results=results, times = days , priors=prior_dict, sigma=10.,gamma=gamma)
+    ts_log_evidence = ttt.log_evidence(h)
+    ttt.convergence(h, iterations=4)
+    ttt_log_evidence = ttt.log_evidence(h)
+    println("Gamma:")
+    println(gamma)
+    println("Evidencia con handicap y komi:")
+    println(ttt_log_evidence)
+end
 
 lc = ttt.learning_curves(h)
-lc["(30.0, 19)"][end][2] 
+lc["(30.0, 19)"][end][2]
