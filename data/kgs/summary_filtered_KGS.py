9d46ce172b03f8b743961a79b73dc808dbb0563d
Martin Amigo <martinamigo@protonmail.com>
2021-06-02 13:03:04 -0300

    filtro para kgs, resultados en gamma, y detalles informe

---
+++ data/kgs/summary_filtered_KGS.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import json
+
+csv_name = './KGS.csv'
+
+print("Dataframe metida")
+df = pd.read_csv(csv_name)
+df = df[['id', 'black', 'white', 'order', 'outcome', 'handicap', 'komi',
+        'width', 'started', 'whiteRank', 'blackRank']]
+
+print("Total: ", df.shape)
+df = df[(df["width"] >= 9) & (df["width"] <= 19)]
+print("Tamaño estandar: ", df.shape)
+df = df[(df.order == 1) | (df.order == 0)]
+print("Resultado estandar: ", df.shape)
+
+df['black_win'] = df.order
+def outcomeOutput(x):
+    if 'Resign' in x:
+        x = 'Resign'
+    elif 'Time' in x:
+        x = 'Time'
+    else:
+        x=x
+
+#pongo cero en los handicap nulos
+df['handicap'] = df['handicap'].map(lambda x : x if x >= 2 else 0)
+#df.outcome = df.outcome.apply(lambda x: outcomeOutput(x))
+df = df[['id', 'black', 'white', 'outcome', 'black_win', 'handicap', 'komi',
+        'width', 'started', 'whiteRank', 'blackRank']]
+# Ordeno
+df = df.sort_values(by=['started', 'id'])
+
+df = df.reset_index()
+df.to_csv("./KGS_filtered.csv", index=False)
