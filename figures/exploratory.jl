ca86e91e842e7dd22b289243a3de35dbe32f4fd5
TobiasCarreira <tobiascarreira@gmail.com>
2021-06-19 14:36:53 -0300

    Algunos graficos y filtros ogs

--- figures/exploratory.jl
+++ figures/exploratory.jl
@@ -2,27 +2,45 @@ using CSV
 using DataFrames
 using VegaLite
 
-aago = CSV.read("../data/aago/aago.csv", DataFrame)
-ogs = CSV.read("../data/ogs/summary.csv", DataFrame)[!, ["order", "handicap", "komi", "rules", "annulled", "ranked", "outcome"]]
-kgs = CSV.read("../data/kgs/KGS.csv", DataFrame)
-
 count(col) = (df -> combine(groupby(df, [col]), nrow => :count))
-barplot(x) = @vlplot(x="count()", y=x) + @vlplot(mark=:bar) + @vlplot(mark={:text, align=:left, dx=4}, text="count()")
-logbarplot(x) = @vlplot(x={"count()", scale={type="log",base=10}}, y=x) + @vlplot(mark=:bar) + @vlplot(mark={:text, align=:left, dx=4}, text="count()")
-verticallogbarplot(col) = (df -> 
+barplot(col,category=:quantitative) = (df ->
     df |> count(col) |>
-    @vlplot(:bar, x=col, y={:count, scale={type="log",base=10}}))
+    @vlplot(x=:count, y={col, type=category}) + @vlplot(mark=:bar) + @vlplot(mark={:text, align=:left, dx=4}, text=:count))
+logbarplot(col,category=:quantitative) = (df ->
+    df |> count(col) |>
+    @vlplot(x={:count, scale={type="log",base=10}}, y={col, type=category}) + @vlplot(mark=:bar) + @vlplot(mark={:text, align=:left, dx=4}, text=:count))
+verticallogbarplot(col,category=:quantitative) = (df -> 
+    df |> count(col) |>
+    @vlplot(:bar, x={col, type=category}, y={:count, scale={type="log",base=10}}))
 heatmap(x,y) = @vlplot(x=x, y=y) + @vlplot(:rect, width={step=40}, height={step=40}, color={"count()", scale={range=["#610066", "#E6DF17"]}}) + @vlplot(mark={:text, color=:white}, text="count()")
 savepdf(filename) = save(filename*".pdf")
 
-aago |> barplot("komi:o") |> savepdf("aago/komi")
-aago |> barplot("handicap:o") |> savepdf("aago/handicap")
-aago |> heatmap("komi:o", "handicap:o") |> savepdf("aago/komi-by-handicap")
+function aagoplots()
+    aago = DataFrame(CSV.File("../data/aago/aago.csv"))
+    aago |> barplot(komi, :ordinal) |> savepdf("aago/komi")
+    aago |> barplot(handicap, :ordinal) |> savepdf("aago/handicap")
+    aago |> heatmap("komi:o", "handicap:o") |> savepdf("aago/komi-by-handicap")
 
-aago |> barplot(:reason) |> savepdf("aago/reason")
+    aago |> barplot(:reason) |> savepdf("aago/reason")
 
-aago |> barplot(:result) |> savepdf("aago/result")
-aago |> heatmap(:result, :reason) |> savepdf("aago/result-by-reason")
+    aago |> barplot(:result) |> savepdf("aago/result")
+    aago |> heatmap(:result, :reason) |> savepdf("aago/result-by-reason")
+end
+function ogsplots()
+    columns = ["order", "handicap", "komi", "rules", "annulled", "ranked", "outcome"]
+    ogs = DataFrame(CSV.File("../data/ogs/summary.csv";select=columns))
+    filters = (0 .<= ogs.handicap .<= 9) .& (.! ogs.annulled) .& ((ogs.order .== "(1, 0)") .| (ogs.order .== "(0, 1)"))
+    ogs = ogs[filters, :]
+    ogs |> verticallogbarplot(:komi) |> savepdf("ogs/komi")
+    ogs |> logbarplot(:handicap, :ordinal) |> savepdf("ogs/handicap")
+end
 
-ogs |> verticallogbarplot(:komi) |> savepdf("ogs/komi")
-ogs |> verticallogbarplot(:handicap) |> savepdf("ogs/handicap")
+function kgsplots()
+    kgs = CSV.read("../data/kgs/KGS.csv", DataFrame)
+end
+
+aagoplots()
+GC.gc()
+ogsplots()
+GC.gc()
+kgsplots()
