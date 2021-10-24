96bf6069cf2667c94c315d05b4352aa641375321
Tobias Carreira Munich <tobiascarreira@gmail.com>
2021-10-24 19:11:24 -0300

    WHR imprimo learning curves

--- estimations/run_whr.py
+++ estimations/run_whr.py
@@ -7,7 +7,7 @@ from argparse import ArgumentParser, FileType
 
 
 class WHRRunner:
-    def __init__(self, dynamic_factor, handicap_elo, matches):
+    def __init__(self, dynamic_factor, handicap_elo, matches, auto_iter_rate):
         """
         @param dynamic_factor: número que indica cuanta varianza hay entre las habilidades de un jugador en el tiempo
         @param handicap_elo: número que indica la habilidad que aporta una piedra de handicap, medido en unidades de elo
@@ -17,6 +17,7 @@ class WHRRunner:
         self.evidence = []
         self.handicap_elo = handicap_elo
         self.matches = matches
+        self.auto_iter_rate = auto_iter_rate
     
     def match_evidence(self, match):
         black_probability, white_probability = self.whr.probability_future_match(match['black'], match['white'],
@@ -37,8 +38,11 @@ class WHRRunner:
             self.whr.create_game(match['black'], match['white'], match['winner'],
                                  match['day'], match['handicap'] * self.handicap_elo)
             self.optimize_players(match)
-            if index % 1 == 0:
+            if index % self.auto_iter_rate == 0:
                 self.whr.auto_iterate(time_limit=10, precision=10E-3)
+        # La incertidumbre se calcula al iterar
+        # Si no se itera al final, puede haber jugadores sin incertidumbre
+        self.whr.auto_iterate(time_limit=10, precision=10E-3)
     
     def cross_entropy(self):
         return -self.log_evidence()/len(self.evidence)
@@ -49,13 +53,27 @@ class WHRRunner:
     def log_evidence(self):
         return reduce(lambda x, acc: x + acc, [log(e) for e in self.evidence])
 
+    def learning_curves(self):
+        data = [
+            (player, day, mean, variance)
+            for player in self.whr.players.keys()
+            for (day, mean, variance) in self.whr.ratings_for_player(player)
+        ]
+        return pd.DataFrame(data, columns=['player', 'day', 'mean', 'variance'])
 
-parser = ArgumentParser(description='Corre el modelo WHR sobre un dataset en CSV.')
-parser.add_argument('dataset', type=FileType('r'), help='CSV con columnas: black, white, handicap, winner, day')
-# parser.add_argument('-o', '--output', dest='output_file', type=FileType('w'), required=True)
-parser.add_argument('-l', '--log', dest='log_file', type=FileType('w'), required=True)
-args = parser.parse_args()
+
+def read_args():
+    parser = ArgumentParser(description='Corre el modelo WHR sobre un dataset en CSV.')
+    parser.add_argument('dataset', type=FileType('r'), help='CSV con columnas: black, white, handicap, winner, day')
+    parser.add_argument('-l', '--learning_curves', dest='learning_curves_file', type=FileType('w'), required=True)
+    parser.add_argument('-r', '--results', dest='results_file', type=FileType('w'), required=True)
+    parser.add_argument('-i', '--auto_iter_rate', dest='auto_iter_rate', type=int, default=100,
+                        help='Cantidad de partidos entre iteraciones del algoritmo de Newton')
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
+    args = read_args()
     df = pd.read_csv(args.dataset)
 
     # TODO: hiperparámetros a optimizar
@@ -63,9 +81,11 @@ if __name__ == "__main__":
     HANDICAP_ELO = 50
     # TODO: el handicap lo pide en unidades de elo, hay que buscar un multiplicador que tenga sentido
     # TODO: se podría agregar komi restándolo al handicap
-    runner = WHRRunner(DYNAMIC_FACTOR, HANDICAP_ELO, df)
+    runner = WHRRunner(DYNAMIC_FACTOR, HANDICAP_ELO, df, args.auto_iter_rate)
     runner.iterate()
 
-    print("geometric_mean:", runner.geometric_mean(), file=args.log_file)  # 0.5369 en AAGO
-    print("log_evidence:", runner.log_evidence(), file=args.log_file)  # -2081.4123902144534 en AAGO
-    print("cross_entropy:", runner.cross_entropy(), file=args.log_file)  # 0.6218740335268759 en AAGO
+    runner.learning_curves().to_csv(args.learning_curves_file, index=False)
+
+    print("geometric_mean:", runner.geometric_mean(), file=args.results_file)  # 0.5369 en AAGO
+    print("log_evidence:", runner.log_evidence(), file=args.results_file)  # -2081.4123902144534 en AAGO
+    print("cross_entropy:", runner.cross_entropy(), file=args.results_file)  # 0.6218740335268759 en AAGO
