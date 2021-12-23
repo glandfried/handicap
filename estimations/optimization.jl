10231606816e2985fa0d996c24ed1ef24110641a
Tobias Carreira Munich <tobiascarreira@gmail.com>
2021-12-23 16:50:22 -0300

    Deshacer cambios que no queria commitear

--- estimations/optimization.jl
+++ estimations/optimization.jl
@@ -29,24 +29,3 @@ println(result)
 println("El mejor gamma es:")
 min = Optim.minimizer(result)
 println(min)
-
-
-prior_dict = init_priors(data, model)
-events, weights = events_weights(data, model)
-sigma = 6.0
-iterations = 16
-
-lc, evidence, dict = run_and_converge(events, results, days, prior_dict, sigma, min, iterations, weights, model)
-generate_csv("lc_aago_ttt-hreg-kreg.csv", prior_dict, lc)
-
-df = DataFrame(player = String[], time = Int64[], mu = Float64[], sigma = Float64[])
-for (time, skill) in lc["222"]
-    push!(df,("222", time, skill.mu, skill.sigma))
-end
-for (time, skill) in lc["176"]
-    push!(df,("176", time, skill.mu, skill.sigma))
-end
-for (time, skill) in lc["218"]
-    push!(df,("218", time, skill.mu, skill.sigma))
-end
-CSV.write("lc_players.csv", df; header=true)
