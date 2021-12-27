8757fd9540e5a3483de2bdc21d194832936acaff
Tobias Carreira Munich <tobiascarreira@gmail.com>
2021-12-27 16:58:57 -0300

    Agrego filtros de AAGO

--- data/aago/adapt.py
+++ data/aago/adapt.py
@@ -3,6 +3,16 @@ from datetime import date
 
 df = pd.read_csv('./aago.csv')
 
+if not (df['komi'].isin([0.5, 6.5])).all():
+    raise Exception('Existe un komi con valor distinto a 0.5 y 6.5, revisar.')
+
+filters = (df['handicap'] >= 0) & (df['handicap'] <= 9) &\
+          (df['result'].isin(['black', 'white'])) &\
+          (df['reason'] != 'walkover') &\
+          (df['unrated'] == 0)
+
+df = df[filters]
+
 df['winner'] = df['result'].apply(lambda result: 'B' if result == 'black' else 'W')
 df['black'] = df['black_player_id']
 df['white'] = df['white_player_id']
