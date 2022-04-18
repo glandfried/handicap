1e52b1d5ea9ec9e3012a9c92b12d635103f54c67
Tobias Carreira Munich <tobiascarreira@gmail.com>
2022-04-18 11:54:40 -0300

    WHR AAGO: nuevo adaptador de datos

--- data/aago/adapt.py
+++ data/aago/adapt.py
@@ -1,25 +1,14 @@
 import pandas as pd
 from datetime import date
 
-df = pd.read_csv('./aago.csv')
+df = pd.read_csv('./aago_original_filtered.csv')
 
-if not (df['komi'].isin([0.5, 6.5])).all():
-    raise Exception('Existe un komi con valor distinto a 0.5 y 6.5, revisar.')
+df['id'] = range(len(df))
+df['winner'] = df['black_win'].apply(lambda black_win: 'B' if black_win else 'W')
 
-filters = (df['handicap'] >= 0) & (df['handicap'] <= 9) &\
-          (df['result'].isin(['black', 'white'])) &\
-          (df['reason'] != 'walkover') &\
-          (df['unrated'] == 0)
-
-df = df[filters]
-
-df['winner'] = df['result'].apply(lambda result: 'B' if result == 'black' else 'W')
-df['black'] = df['black_player_id']
-df['white'] = df['white_player_id']
-
-df['date'] = df['date'].apply(date.fromisoformat)
+df['date'] = df['end_date'].apply(date.fromisoformat)
 first_day = df['date'].min()
 df['day'] = df['date'].apply(lambda d: (d - first_day).days + 1)
 
 columns = ['id', 'black', 'white', 'handicap', 'komi', 'winner', 'day']
-df.sort_values('day').to_csv('./aago.adapted.csv', columns=columns, index=False)
+df.sort_values('day').to_csv('./aago_original_filtered.adapted.csv', columns=columns, index=False)
